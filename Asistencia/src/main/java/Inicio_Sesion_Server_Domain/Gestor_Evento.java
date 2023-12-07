62b65d5931ff8b561ea05b40b196f1eefc21cb92
JorgeArroyo33 <146016319+JorgeArroyo33@users.noreply.github.com>
2023-12-07 18:31:29 +0100

    Delete Asistencia directory

--- Asistencia/src/main/java/Inicio_Sesion_Server_Domain/Gestor_Evento.java
+++
@@ -1,4 +0,0 @@
-package Inicio_Sesion_Server_Domain;
-
-public class Gestor_Evento extends Usuario {
-}
--- Asistencia/src/main/java/Inicio_Sesion_Server_Domain/Organizador_Evento.java
+++
@@ -1,4 +0,0 @@
-package Inicio_Sesion_Server_Domain;
-
-public class Organizador_Evento extends Usuario {
-}
