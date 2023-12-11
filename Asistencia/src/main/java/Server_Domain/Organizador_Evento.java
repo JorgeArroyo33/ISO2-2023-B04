f69897a6518ecf50e2ccfb57ecfaafc696b41bc9
SergiooCoorne <134279611+SergiooCoorne@users.noreply.github.com>
2023-12-11 16:59:35 +0100

    Implementación del lado del servidor

--- Asistencia/src/main/java/Server_Domain/Organizador_Evento.java
+++ Asistencia/src/main/java/Server_Domain/Organizador_Evento.java
@@ -1,4 +1,16 @@
-package Inicio_Sesión.Servidor.Server_Domain;
+package Gestionar_Eventos.Servidor.Server_Eventos.Server_Domain;
 
-public class Organizador_Evento extends Usuario {
+public class Organizador_Evento {
+
+	private Evento[] eventosAbiertos;
+
+	/**
+	 * 
+	 * @param evento
+	 */
+	public void addEvento(Evento evento) {
+		// TODO - implement Organizador_Evento.addEvento
+		throw new UnsupportedOperationException();
+	}
+
 }
