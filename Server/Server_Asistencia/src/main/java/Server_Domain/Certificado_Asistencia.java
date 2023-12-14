0446117972d095e9c308475f1dd359ff04b7e3bf
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-14 08:47:35 +0100

    Server_Asitencia V0.0.1

---
+++ Server/Server_Asistencia/src/main/java/Server_Domain/Certificado_Asistencia.java
@@ -0,0 +1,13 @@
+package Server_Domain;
+
+import Todos_Usuarios.Inicio_Sesi�n.Servidor.Server_Domain.*;
+import Servidor.Server_Eventos.Server_Domain.*;
+
+public class Certificado_Asistencia {
+
+	private String idCertificado;
+	private Usuario alumno;
+	private Evento evento;
+	private Date fecha;
+
+}
