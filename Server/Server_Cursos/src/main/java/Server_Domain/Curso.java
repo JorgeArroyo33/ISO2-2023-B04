34f840a9a67120351e1016232f8c5e7489ad7648
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-14 09:50:12 +0100

    Server_Cursos V0.0.1 (Resubida ya que se subio de forma incorrecta)

---
+++ Server/Server_Cursos/src/main/java/Server_Domain/Curso.java
@@ -0,0 +1,13 @@
+package Servidor.Server_Cursos.Server_Domain;
+
+import Todos_Usuarios.Inicio_Sesión.Servidor.Server_Domain.*;
+
+public class Curso {
+
+	private String idCurso;
+	private Usuario profesor;
+	private Usuario[] alumnos;
+	private String nombre;
+	private String descripcion;
+
+}
