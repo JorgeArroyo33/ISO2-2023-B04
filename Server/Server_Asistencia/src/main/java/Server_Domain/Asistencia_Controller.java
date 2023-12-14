fedc2f2def5c799c8f6162ab60f34b334b1a4d94
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-14 10:23:07 +0100

    Resubida del modulo de Server_Asistenica V 0.0.1 porque daba fallos

---
+++ Server/Server_Asistencia/src/main/java/Server_Domain/Asistencia_Controller.java
@@ -0,0 +1,14 @@
+package Servidor.Server_Asistencia.Server_Domain;
+
+public class Asistencia_Controller {
+
+	/**
+	 * 
+	 * @param idEvento
+	 */
+	public Certificado solicitar_certificado(String idEvento) {
+		// TODO - implement Asistencia_Controller.solicitar_certificado
+		throw new UnsupportedOperationException();
+	}
+
+}
