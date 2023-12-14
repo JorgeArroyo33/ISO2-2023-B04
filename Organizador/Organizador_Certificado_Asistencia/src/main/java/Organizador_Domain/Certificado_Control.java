7d13a024651b0ff51cd2c68437cfb4a95aebabc8
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-14 10:45:03 +0100

    Modulo Organizador_Certificado_Asistenica V0.0.1

---
+++ Organizador/Organizador_Certificado_Asistencia/src/main/java/Organizador_Domain/Certificado_Control.java
@@ -0,0 +1,24 @@
+package Organizador.Organizador_Certificado_Asistencia.Organizador_Domain;
+
+public class Certificado_Control {
+
+	/**
+	 * 
+	 * @param idEvento
+	 */
+	public void solicitar_certificado(String idEvento) {
+		// TODO - implement Certificado_Control.solicitar_certificado
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param idEvento
+	 * @param firma
+	 */
+	private Certificado firmar_certificado(String idEvento, String firma) {
+		// TODO - implement Certificado_Control.firmar_certificado
+		throw new UnsupportedOperationException();
+	}
+
+}
