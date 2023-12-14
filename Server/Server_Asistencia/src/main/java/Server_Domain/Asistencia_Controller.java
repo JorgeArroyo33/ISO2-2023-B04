c4232abc0249701cf78638147d321a048e0bf8dd
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-14 08:58:56 +0100

    Server_Asistencia V0.0.1

---
+++ Server/Server_Asistencia/src/main/java/Server_Domain/Asistencia_Controller.java
@@ -0,0 +1,14 @@
+package Server_Domain;
+
+public class Asistencia_Controller {
+
+	/**
+	 * 
+	 * @param idEvento
+	 */
+	public Certificado solicitar_certificado(String idEvento) {
+		// TODO - implement Asistencia_Controller.solicitar_certificado
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Server/Server_Asistencia/src/main/java/Server_Presentacion/AsistenciaInterface.java
@@ -0,0 +1,14 @@
+package Server_Presentacion;
+
+public class AsistenciaInterface {
+
+	/**
+	 * 
+	 * @param idEvento
+	 */
+	public Certificado solicitar_certificado(String idEvento) {
+		// TODO - implement AsistenciaInterface.solicitar_certificado
+		throw new UnsupportedOperationException();
+	}
+
+}
