1394de12e11cc2f3c7e42f1852f2b475d262ad47
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-15 13:39:42 +0100

    Modulo Asistente_Asistencia y funcionalidad confirmacion_asistencia

---
+++ Asistente/Asistente_Asistencia/src/main/java/Asistente_Domain/Asistencia_Control.java
@@ -0,0 +1,34 @@
+package Asistente.Asistente_Asistencia.Asistente_Domain;
+
+public class Asistencia_Control {
+
+	/**
+	 * 
+	 * @param loginAsistente
+	 * @param QR
+	 */
+	public void confirmar_asistencia_QR(String loginAsistente, String QR) {
+		// TODO - implement Asistencia_Control.confirmar_asistencia_QR
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param loginAsistente
+	 * @param numero
+	 */
+	public void confirmar_asistencia_numero(String loginAsistente, int numero) {
+		// TODO - implement Asistencia_Control.confirmar_asistencia_numero
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param loginAsistente
+	 */
+	public void confirmar_asistencia_bluetooth(String loginAsistente) {
+		// TODO - implement Asistencia_Control.confirmar_asistencia_bluetooth
+		throw new UnsupportedOperationException();
+	}
+
+}
