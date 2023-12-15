9841d54682f300c43247af4208c63b7b6a1f6c98
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-15 13:28:40 +0100

    Funcionalidad confirmacion_asistenica_server

--- Server/Server_Asistencia/src/main/java/Server_Domain/Asistencia_Controller.java
+++ Server/Server_Asistencia/src/main/java/Server_Domain/Asistencia_Controller.java
@@ -11,4 +11,42 @@ public class Asistencia_Controller {
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * 
+	 * @param idCertificado
+	 */
+	private void almacenarCertificado(String idCertificado) {
+		// TODO - implement Asistencia_Controller.almacenarCertificado
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param loginAsistente
+	 */
+	public void confirmar_asistencia_bluetooth(String loginAsistente) {
+		// TODO - implement Asistencia_Controller.confirmar_asistencia_bluetooth
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param loginAsistente
+	 * @param QR
+	 */
+	public void confirmar_asistencia_QR(String loginAsistente, String QR) {
+		// TODO - implement Asistencia_Controller.confirmar_asistencia_QR
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param loginAsistente
+	 * @param numero
+	 */
+	public void confirmar_asistencia_numero(String loginAsistente, int numero) {
+		// TODO - implement Asistencia_Controller.confirmar_asistencia_numero
+		throw new UnsupportedOperationException();
+	}
+
 }
--- Server/Server_Asistencia/src/main/java/Server_Presentacion/AsistenciaInterface.java
+++ Server/Server_Asistencia/src/main/java/Server_Presentacion/AsistenciaInterface.java
@@ -11,4 +11,33 @@ public class AsistenciaInterface {
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * 
+	 * @param loginAsistente
+	 */
+	public void confirmar_asistencia_bluetooth(String loginAsistente) {
+		// TODO - implement AsistenciaInterface.confirmar_asistencia_bluetooth
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param loginAsistente
+	 * @param QR
+	 */
+	public void confirmar_asistencia_QR(String loginAsistente, String QR) {
+		// TODO - implement AsistenciaInterface.confirmar_asistencia_QR
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param loginAsistente
+	 * @param numero
+	 */
+	public void confirmar_asistencia_numero(String loginAsistente, int numero) {
+		// TODO - implement AsistenciaInterface.confirmar_asistencia_numero
+		throw new UnsupportedOperationException();
+	}
+
 }
