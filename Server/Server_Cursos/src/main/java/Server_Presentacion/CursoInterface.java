645e7260a07cb99b5dcb2429141793ea2f5a2652
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-12-15 13:18:55 +0100

    Creacion modulo Server_Cursos y asignar_persona_curso_server

---
+++ Server/Server_Cursos/src/main/java/Server_Presentacion/CursoInterface.java
@@ -0,0 +1,50 @@
+package Servidor.Server_Cursos.Server_Presentacion;
+
+import Servidor.Server_Cursos.Server_Domain.*;
+
+public class CursoInterface {
+
+	/**
+	 * 
+	 * @param loginAlumno
+	 * @param idCurso
+	 */
+	public void asignar_alumno_curso(String loginAlumno, String idCurso) {
+		// TODO - implement CursoInterface.asignar_alumno_curso
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param loginProfesor
+	 * @param idCurso
+	 */
+	public void asignar_profesor_curso(String loginProfesor, String idCurso) {
+		// TODO - implement CursoInterface.asignar_profesor_curso
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param idCurso
+	 */
+	public String[] get_alumnos_curso(String idCurso) {
+		// TODO - implement CursoInterface.get_alumnos_curso
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param idCurso
+	 */
+	public String[] get_profesor_curso(String idCurso) {
+		// TODO - implement CursoInterface.get_profesor_curso
+		throw new UnsupportedOperationException();
+	}
+
+	public Curso[] get_cursos() {
+		// TODO - implement CursoInterface.get_cursos
+		throw new UnsupportedOperationException();
+	}
+
+}
